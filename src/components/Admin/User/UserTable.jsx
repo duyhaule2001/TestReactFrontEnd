5d4935012548418b01490522bd252e5d5ac21a98
duyhaule2001 <duyhaule2001@gmail.com>
2024-08-25 14:04:17 +0900

    Bài Tập Filter Danh Sách User

--- src/components/Admin/User/UserTable.jsx
+++ src/components/Admin/User/UserTable.jsx
@@ -1,102 +1,107 @@
-import React, { useEffect, useState } from 'react';
-import { Table, Row, Col } from 'antd';
-import InputSearch from './InputSearch';
-import { callFetchListUser } from '../../../services/api';
+import React, { useEffect, useState } from "react";
+import { Table, Row, Col } from "antd";
+import InputSearch from "./InputSearch";
+import { callFetchListUser } from "../../../services/api";
 
-// https://stackblitz.com/run?file=demo.tsx
 const UserTable = () => {
-    const [listUser, setListUser] = useState([]);
-    const [current, setCurrent] = useState(1);
-    const [pageSize, setPageSize] = useState(5);
-    const [total, setTotal] = useState(0);
+  const [listUser, setListUser] = useState([]);
+  const [current, setCurrent] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-    // useEffect(() => {
-    //     fetchUser();
-    // }, []);
+  useEffect(() => {
+    fetchUser();
+  }, [current, pageSize]);
 
-    useEffect(() => {
-        fetchUser();
-    }, [current, pageSize]);
+  const fetchUser = async (searchFilter) => {
+    setLoading(true);
+    let query = `current=${current}&pageSize=${pageSize}`;
 
-    const fetchUser = async () => {
-        const query = `current=${current}&pageSize=${pageSize}`;
-        const res = await callFetchListUser(query);
-        if (res && res.data) {
-            setListUser(res.data.result);
-            setTotal(res.data.meta.total)
-        }
+    if (searchFilter) {
+      query += `&${searchFilter}`;
     }
 
-    const columns = [
-        {
-            title: 'Id',
-            dataIndex: '_id',
-        },
-        {
-            title: 'Tên hiển thị',
-            dataIndex: 'fullName',
-            sorter: true
-        },
-        {
-            title: 'Email',
-            dataIndex: 'email',
-            sorter: true,
-        },
-        {
-            title: 'Số điện thoại',
-            dataIndex: 'phone',
-            sorter: true
-        },
-        {
-            title: 'Action',
-            render: (text, record, index) => {
-                return (
-                    <><button>Delete</button></>
-                )
-            }
-        }
-    ];
+    const res = await callFetchListUser(query);
+    if (res && res.data) {
+      setListUser(res.data.result);
+      setTotal(res.data.meta.total);
+    }
+    setLoading(false);
+  };
 
+  const onChange = (pagination, filters, sorter, extra) => {
+    if (pagination && pagination.current !== current) {
+      setCurrent(pagination.current);
+    }
+    if (pagination && pagination.pageSize !== pageSize) {
+      setPageSize(pagination.pageSize);
+      setCurrent(1);
+    }
+    console.log("params", pagination, filters, sorter, extra);
+  };
 
+  const handleSearch = (query) => {
+    fetchUser(query);
+  };
 
-    const onChange = (pagination, filters, sorter, extra) => {
-        if (pagination && pagination.current !== current) {
-            setCurrent(pagination.current)
-        }
-        if (pagination && pagination.pageSize !== pageSize) {
-            setPageSize(pagination.pageSize)
-            setCurrent(1);
-        }
-        console.log('params', pagination, filters, sorter, extra);
-    };
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "_id",
+    },
+    {
+      title: "Tên hiển thị",
+      dataIndex: "fullName",
+      sorter: true,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      sorter: true,
+    },
+    {
+      title: "Số điện thoại",
+      dataIndex: "phone",
+      sorter: true,
+    },
+    {
+      title: "Action",
+      render: (text, record, index) => {
+        return (
+          <>
+            <button>Delete</button>
+          </>
+        );
+      },
+    },
+  ];
 
-    return (
-        <>
-            <Row gutter={[20, 20]}>
-                <Col span={24}>
-                    <InputSearch />
-                </Col>
-                <Col span={24}>
-                    <Table
-                        className='def'
-                        columns={columns}
-                        dataSource={listUser}
-                        onChange={onChange}
-                        rowKey="_id"
-                        pagination={
-                            {
-                                current: current,
-                                pageSize: pageSize,
-                                showSizeChanger: true,
-                                total: total
-                            }
-                        }
-                    />
-                </Col>
-            </Row>
-        </>
-    )
-}
-
+  return (
+    <>
+      <Row gutter={[20, 20]}>
+        <Col span={24}>
+          <InputSearch handleSearch={handleSearch} />
+        </Col>
+        <Col span={24}>
+          <Table
+            className="def"
+            columns={columns}
+            dataSource={listUser}
+            onChange={onChange}
+            rowKey="_id"
+            loading={loading}
+            pagination={{
+              current: current,
+              pageSize: pageSize,
+              showSizeChanger: true,
+              total: total,
+            }}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default UserTable;
