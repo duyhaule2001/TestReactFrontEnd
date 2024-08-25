4e62ab85ac193054bcd564feead041a689d49f31
duyhaule2001 <duyhaule2001@gmail.com>
2024-08-25 13:02:59 +0900

    Hiển thị danh sách người dùng, chia trang

---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+import axios from '../utils/axios-customize';
+
+export const callRegister = (fullName, email, password, phone) => {
+    return axios.post('/api/v1/user/register', { fullName, email, password, phone })
+}
+
+export const callLogin = (username, password) => {
+    return axios.post('/api/v1/auth/login', { username, password })
+}
+
+export const callFetchAccount = () => {
+    return axios.get('/api/v1/auth/account')
+}
+
+export const callLogout = () => {
+    return axios.post('/api/v1/auth/logout')
+}
+
+export const callFetchListUser = (query) => {
+    // current=1&pageSize=3
+    return axios.get(`/api/v1/user?${query}`)
+}
